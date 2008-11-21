d2698c87dfd4ae33e75051c93ce5f41a8c4da5a5
Alex Plotnick <shrike@netaxs.com>
2008-11-21 16:52:09 +0000

    Major reorganization and optimization: Oppen's scan & output routines are gone, but their essence remains, though in a fragmented form.  Tokens are now responsible for outputing themselves, and the public methods of PrettyPrinter now handle the work of scan.
    git-svn-id: file:///home/alex/svn/src/prettyprinter/trunk@679 1fe77fbb-760c-0410-9197-9ee16f777b24

--- prettyprinter.py
+++ prettyprinter.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 import sys
 from collections import deque
 
@@ -5,40 +6,78 @@ __all__ = ["PrettyPrinter", "pprint"]
 
 class Token(object):
     """Base class for prettyprinter tokens."""
-    pass
-
-class Newline(Token):
-    def __init__(self, mandatory=False, fill=False, blankspace=0, offset=0):
-        self.mandatory = mandatory
-        self.fill = fill
-        self.blankspace = blankspace
-        self.offset = offset
 
 class Begin(Token):
-    """Begin a logical block.  If the offset is omitted or is None, use the
-    length of the first string in the block."""
+    """Begin a logical block."""
 
-    def __init__(self, offset=None, prefix=None):
+    def __init__(self, offset=0, prefix=None):
         self.offset = offset
         self.prefix = prefix
+        self.size = 0
+
+    def output(self, pp):
+        if self.prefix:
+            pp.stream.write(self.prefix)
+        pp.printstack.append((pp.space - (self.offset or 0),
+                              self.size <= pp.space))
 
 class End(Token):
     """End a logical block."""
 
     def __init__(self, suffix=None):
         self.suffix = suffix
+        self.size = 0
+
+    def output(self, pp):
+        if self.suffix:
+            pp.stream.write(self.suffix)
+        if pp.printstack:
+            pp.printstack.pop()
+
+class Newline(Token):
+    def __init__(self, mandatory=False, fill=False, offset=0):
+        self.mandatory = mandatory
+        self.fill = fill
+        self.offset = offset
+
+    def output(self, pp):
+        (block_offset, fits) = pp.printstack[-1]
+        if self.mandatory:
+            pp.space = block_offset - self.offset
+            pp.stream.write("\n" + " " * (pp.margin - pp.space))
+        elif fits:
+            # Entire block fits; don't break line.
+            pass
+        elif self.fill:
+            # Fill-style (a.k.a. block-style) newline.
+            if self.size > pp.space:
+                pp.space = block_offset - self.offset
+                pp.stream.write("\n" + " " * (pp.margin - pp.space))
+        else:
+            # Linear-style newline.
+            self.space = block_offset - self.offset
+            pp.stream.write("\n" + " " * (pp.margin - pp.space))
+
+class String(Token):
+    def __init__(self, string, size):
+        self.string = string
+        self.size = size
+
+    def output(self, pp):
+        pp.stream.write(self.string)
+        pp.space -= self.size
 
 class LogicalBlock(object):
     """A context manager for logical blocks."""
 
     def __init__(self, pp, lst, *args, **kwargs):
-        if not isinstance(pp, PrettyPrinter):
-            raise TypeError("not a pretty-printer")
-        elif not (lst is None or isinstance(lst, (list, tuple))):
-            raise TypeError("invalid logical block list")
+        assert isinstance(pp, PrettyPrinter), "not a pretty-printer"
+        assert lst is None or isinstance(lst, (list, tuple)), \
+            "invalid logical block list"
 
         self.pp = pp
         self.list = lst
+        self.len = len(lst) if lst else 0
         self.suffix = kwargs.pop("suffix", None)
         self.args = args
         self.kwargs = kwargs
@@ -57,59 +96,44 @@ class LogicalBlock(object):
         return self
 
     def next(self):
-        if self.list is None or self.index == len(self.list):
+        index = self.index
+        if index == self.len:
             raise StopIteration
-        value = self.list[self.index]
-        self.index += 1
+        value = self.list[index]
+        self.index = index + 1
         return value
 
     def exit_if_list_exhausted(self):
-        if self.list is None or self.index == len(self.list):
+        if self.index == self.len:
             raise StopIteration
 
-class QueueEntry(object):
-    def __init__(self, x, l): self.token = x; self.size = l
-    def __len__(self): return self.size
-
 class PrettyPrinter(object):
-    infinity = 999999
-
-    def __init__(self, width=80, file=sys.stdout):
+    def __init__(self, width=80, stream=sys.stdout):
         self.margin = int(width)
         if self.margin <= 0:
             raise ValueError("margin must be positive")
-        if not file:
+        if not stream:
             raise RuntimeError("prettyprinting to nowhere")
-        self.file = file
+        self.stream = stream
         self.space = self.margin
         self.scanstack = deque()
         self.printstack = deque()
         self.queue = deque()
+        self.enqueue = self.queue.append
         self.closed = False
 
-    def write(self, str):
-        self._scan(str)
-
-    def newline(self, *args, **kwargs):
-        self._scan(Newline(*args, **kwargs))
-
-    def begin(self, *args, **kwargs):
-        self._scan(Begin(*args, **kwargs))
-
-    def end(self, *args, **kwargs):
-        self._scan(End(*args, **kwargs))
-
-    def logical_block(self, list=None, *args, **kwargs):
-        return LogicalBlock(self, list, *args, **kwargs)
-
-    def pprint(self, obj):
+    def write(self, obj):
         from format import format
 
-        if isinstance(obj, list):
+        if isinstance(obj, basestring):
+            self.string(obj)
+        elif isinstance(obj, (int, float, long, complex)):
+            self.string(str(obj))
+        elif isinstance(obj, list):
             format(self, "~:<~@{~W~^, ~:_~}~:>", obj)
         elif isinstance(obj, tuple):
             format(self, "~<(~;~W,~^ ~:_~@{~W~^, ~:_~}~;)~:>", obj)
-        elif isinstance(obj, (set, frozenset)):
+        elif isinstance(obj, (set, frozenset, deque)):
             format(self, "~A~<([~;~@{~W~^, ~:_~}~;])~:>",
                    type(obj).__name__, list(obj))
         elif isinstance(obj, dict):
@@ -117,113 +141,85 @@ class PrettyPrinter(object):
         elif hasattr(obj, "__pprint__"):
             obj.__pprint__(self)
         else:
-            self.write(repr(obj))
+            self.string(str(obj))
+
+    def begin(self, *args, **kwargs):
+        stack = self.scanstack
+        if not stack:
+            self.leftotal = self.rightotal = 1
+            assert not self.queue, "queue should be empty"
+        tok = Begin(*args, **kwargs)
+        tok.size = -self.rightotal
+        self.enqueue(tok)
+        stack.append(tok)
+
+    def end(self, *args, **kwargs):
+        tok = End(*args, **kwargs)
+        stack = self.scanstack
+        if not stack:
+            tok.output(self)
+        else:
+            self.enqueue(tok)
+            top = stack.pop()
+            top.size += self.rightotal
+            if isinstance(top, Newline) and stack:
+                top = stack.pop()
+                top.size += self.rightotal
+            if not stack:
+                self.flush()
+
+    def newline(self, *args, **kwargs):
+        stack = self.scanstack
+        if not stack:
+            self.leftotal = self.rightotal = 1
+            assert not self.queue, "queue should be empty"
+        else:
+            top = stack[-1]
+            if isinstance(top, Newline):
+                top.size += self.rightotal
+                stack.pop()
+        tok = Newline(*args, **kwargs)
+        tok.size = -self.rightotal
+        self.enqueue(tok)
+        stack.append(tok)
+
+    def string(self, s):
+        l = len(s)
+        stack = self.scanstack
+        if not stack:
+            self.stream.write(s)
+            self.space -= l
+        else:
+            q = self.queue[-1]
+            if isinstance(q, String):
+                # Don't create a seperate token; merge with the last one.
+                q.string += s
+                q.size += l
+            else:
+                tok = String(s, l)
+                self.enqueue(tok)
+            self.rightotal += l
+            while self.rightotal - self.leftotal > self.space:
+                stack.popleft().size = 999999   # infinity
+                self.flush()
+
+    def logical_block(self, list=None, *args, **kwargs):
+        return LogicalBlock(self, list, *args, **kwargs)
+
+    def flush(self):
+        """Output as many queue entries as possible."""
+        queue = self.queue
+        while queue and queue[0].size >= 0:
+            q = queue.popleft()
+            q.output(self)
+            self.leftotal += q.size
 
     def close(self):
         if not self.closed:
-            assert len(self.queue) == 0, "leftover items in output queue"
-            assert len(self.scanstack) == 0, "leftover itmes on scan stack"
-            assert len(self.printstack) == 0, "leftover items on print stack"
+            assert not self.queue, "leftover items in output queue"
+            assert not self.scanstack, "leftover itmes on scan stack"
+            assert not self.printstack, "leftover items on print stack"
             self.closed = True
 
-    def _scan(self, x):
-        def push(x): self.scanstack.append(x)
-        def pop(): return self.scanstack.pop()
-        def top(): return self.scanstack[-1]
-        def popbottom(): return self.scanstack.popleft()
-        def empty(): return len(self.scanstack) == 0
-
-        def enqueue(x, l):
-            q = QueueEntry(x, l);
-            self.queue.append(q);
-            return q
-
-        def flushqueue():
-            """Output as many queue entries as possible."""
-            while len(self.queue) > 0 and self.queue[0].size >= 0:
-                q = self.queue.popleft()
-                (x, l) = (q.token, q.size)
-                self._output(x, l)
-                self.leftotal += x.blankspace if isinstance(x, Newline) else l
-
-        def reset():
-            """Reset the size totals and ensure that the queue is empty."""
-            self.leftotal = self.rightotal = 1
-            assert len(self.queue) == 0, "queue should be empty"
-
-        if isinstance(x, Begin):
-            if empty(): reset()
-            push(enqueue(x, -self.rightotal))
-        elif isinstance(x, End):
-            if empty(): self._output(x, 0)
-            else:
-                enqueue(x, 0)
-                q = pop()
-                q.size += self.rightotal
-                if isinstance(q.token, Newline) and not empty():
-                    q = pop()
-                    q.size += self.rightotal
-                if empty(): flushqueue()
-        elif isinstance(x, Newline):
-            if empty(): reset()
-            else:
-                q = top()
-                if isinstance(q.token, Newline):
-                    q.size += self.rightotal
-                    pop()
-            push(enqueue(x, -self.rightotal))
-            self.rightotal += x.blankspace
-        elif isinstance(x, basestring):
-            if empty(): self._output(x, len(x))
-            else:
-                t = top().token
-                if isinstance(t, Begin) and t.offset is None:
-                    t.offset = len(x)
-
-                enqueue(x, len(x))
-                self.rightotal += len(x)
-                while self.rightotal - self.leftotal > self.space:
-                    popbottom().size = self.infinity
-                    flushqueue()
-        else:
-            raise TypeError, "non-token"
-
-    def _output(self, x, l):
-        def push(x): self.printstack.append(x)
-        def pop(): return self.printstack.pop()
-        def top(): return self.printstack[-1]
-
-        def indent(n, newline=False):
-            if newline: self.file.write("\n")
-            self.file.write(n * " ")
-
-        if isinstance(x, Begin):
-            if x.prefix: self._output(x.prefix, len(x.prefix))
-            push((self.space - (x.offset or 0), l <= self.space))
-        elif isinstance(x, End):
-            if x.suffix: self._output(x.suffix, len(x.suffix))
-            pop()
-        elif isinstance(x, Newline):
-            (block_offset, fits) = top()
-            if x.mandatory:
-                self.space = block_offset - x.offset
-                indent(self.margin - self.space, True)
-            elif fits:
-                # Entire block fits; don't break line.
-                self.space -= x.blankspace
-                indent(x.blankspace)
-            elif x.fill:
-                # Fill- or block-style newline.
-                if l > self.space:
-                    self.space = block_offset - x.offset
-                    indent(self.margin - self.space, True)
-            else:
-                # Linear-style newline.
-                self.space = block_offset - x.offset
-                indent(self.margin - self.space, True)
-        elif isinstance(x, basestring):
-            self.file.write(x)
-            self.space -= l
-
 def pprint(obj, *args, **kwargs):
-    PrettyPrinter(*args, **kwargs).pprint(obj)
+    PrettyPrinter(*args, **kwargs).write(obj)
